5b3205ea094ffcf44590599bd9b4a4b253032346
Retardpro <67966971+Retardpro@users.noreply.github.com>
2025-04-18 14:47:49 +0530

    Update twrp_tank.mk

--- twrp_tank.mk
+++ twrp_tank.mk
@@ -16,18 +16,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Inherit some common Orange Fox stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from eqe device
-$(call inherit-product, device/motorola/eqe/device.mk)
+# Inherit from tank device
+$(call inherit-product, device/motorola/tank/device.mk)
 
-PRODUCT_DEVICE := eqe
-PRODUCT_NAME := twrp_eqe
+PRODUCT_DEVICE := tank
+PRODUCT_NAME := twrp_tank
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := motorola edge 50 pro
+PRODUCT_MODEL := motorola edge 50
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="eqe_g-user 13 U2UM34.27-38-5 f759b8 test-keys"
+    PRIVATE_BUILD_DESC="tank-user 12 U3UWS34.67-79-4-1 59658 test-keys"
 
-BUILD_FINGERPRINT := motorola/eqe_g/eqe:13/U2UM34.27-38-5/f759b8:user/release-keys
+BUILD_FINGERPRINT := motorola/tank/tank:12/U3UWS34.67-79-4-1/59658:user/release-keys
