355e854150d699d163cf65ff1bad86531f5ab681
Nick Reuter <nreuter85@gmail.com>
2026-06-04 00:43:02 -0500

    avatrn: Update blobs from avatrn V1UANS35H.98-24-7
    Change-Id: I6aefb843d27cfd32bd22a9668c2c1f16e3c6d893

--- lineage_avatrn.mk
+++ lineage_avatrn.mk
@@ -35,5 +35,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct="avatrn_g" \
-    BuildDesc="avatrn_g-user 15 V1UANS35H.98-24-1 c2817b-28fd7 release-keys" \
-    BuildFingerprint="motorola/avatrn_g/avatrn:15/V1UANS35H.98-24-1/c2817b-28fd7:user/release-keys"
+    BuildDesc="avatrn_g-user 15 V1UANS35H.98-24-7 6bffa1-3b9e8 release-keys" \
+    BuildFingerprint="motorola/avatrn_g/avatrn:15/V1UANS35H.98-24-7/6bffa1-3b9e8:user/release-keys"
